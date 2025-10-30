6366806566944c8e6ca0578838f7b394011a42bc
chandan <github.chintu02@gmail.com>
2025-10-30 16:35:19 +0530

    changes

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "source-bucket-dxv"
-    key = "day-4/terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true    #to use s3 native locking
+    bucket         = "source-bucket-dxv"
+    key            = "day-4/terraform.tfstate"
+    region         = "us-east-1"
+    use_lockfile   = true
+    dynamodb_table = "terraform-"  # ✅ Must match actual table name
+    encrypt        = true
   }
 }
