24577eba6f93de071503fea23c515699cbdafdd7
chandan <github.chintu02@gmail.com>
2025-10-31 17:03:49 +0530

    lifecycle-concept

---
+++ Day-5-Lifecycle-rule/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "name" {
+  ami = "ami-0bdd88bd06d16ba03"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Lifecycle"
+  }
+
+lifecycle {
+  # Enables replacement by creating new before destroying old
+  create_before_destroy = true
+
+  # Ignores changes to tags to avoid unnecessary updates
+  ignore_changes        = [tags]
+
+  # Prevents accidental deletion of the resource
+  prevent_destroy       = true
+}
+}
