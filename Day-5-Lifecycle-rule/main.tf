0505695a78223cb05c8620f90547a7158023d81b
chandan <github.chintu02@gmail.com>
2025-11-01 13:28:41 +0530

    modify

--- Day-5-Lifecycle-rule/main.tf
+++ Day-5-Lifecycle-rule/main.tf
@@ -6,13 +6,34 @@ resource "aws_instance" "name" {
   }
 
 lifecycle {
-  # Enables replacement by creating new before destroying old
-  #create_before_destroy = true
 
+  # -----create befote destroy--------
+  # Enables replacement by creating new before destroying old
+  # create_before_destroy = true
+
+
+
+# --------ignore changes ---------
   # Ignores changes to tags to avoid unnecessary updates
   #ignore_changes        = [tags]
 
+# Examples:
+  ### if you can change tags inside value, this ingnore lifecycle rule can ignore/skip the value, what you add value 
+  ##  ex: tags{
+  #         Name = "cloud"
+  #               / "dev"
+  #   }
+  #   result: Name = "cloud"
+  # this ingnore lifecycle rule can ignore/skip the "dev" value 
+
+
+
+
+# --------prevent destroy -------
   # Prevents accidental deletion of the resource
- # prevent_destroy       = true
+  # prevent_destroy       = true
+
+  # it means you can enable this option instance not terminated,
+  # if u can't terminate instnce until when you disable this option 
 }
 }
